693e7473060f0da1a09854f378c737d90c8ce8fc
Jonathan Cameron <jmcameron@gmail.com>
2013-04-24 09:56:38 -0700

    Added hack to reduce initila screen flash from Vivek Haldar

--- init.el
+++ init.el
@@ -31,6 +31,16 @@
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
 
+;; Hack to reduce initial flashing from Vivek Haldar (http://wattheemacsd.com)
+;;
+;; Turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+;;
+;; No splash screen please ...
+(setq inhibit-startup-message t)
+
 ;;; Code:
 
 (message "Prelude is powering up... Be patient, Master %s!" (getenv "USER"))
